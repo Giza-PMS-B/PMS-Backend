ca17d697dd41c9bbd97807169f6a6aea84f3cd2b
linams <linamohamed1782002@gmail.com>
2025-12-20 22:00:15 +0200

    separate leaf and parent DTOs and Validators

--- Site.Application/DTO/CreateSiteDTO.cs
+++ Site.Application/DTO/CreateSiteDTO.cs
@@ -7,10 +7,8 @@ public class CreateSiteDTO
     public string Path { get; set; }
     public string NameEn { get; set; }
     public string NameAr { get; set; }
-    public decimal? PricePerHour { get; set; }
-    public string IntegrationCode { get; set; }
-    public int? NumberOfSolts { get; set; }
+   
     public bool IsLeaf { get; set; }
     public Guid? ParentId { get; set; }
-    public List<CreatePolygonDTO>? Polygons { get; set; } = [];
+    
 }
