2668286d6e4c9750237ad63776b6779198d5d535
sasa <mostafayounis630@gmail.com>
2025-12-23 02:00:21 +0200

    Ticket Created Reached Invoice via Kafka

--- Booking.Application/Services/TicketService.cs
+++ Booking.Application/Services/TicketService.cs
@@ -1,6 +1,9 @@
 using Booking.Application.DTO;
 using Booking.Model.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using SharedKernel.EventDriven.Abstraction;
 using SharedKernel.Infrastructure.Persistent.Abstraction;
 
 namespace Booking.Application.Services;
@@ -10,12 +13,16 @@ public class TicketService
     private readonly IRepo<Ticket> _ticketRepository;
     private readonly IRepo<Model.Entities.Site> _siteRepository;
     private readonly IUOW _uow;
+    private readonly ILogger<TicketService> _logger;
+    private readonly IIntegrationEventProducer _eventProducer;
 
-    public TicketService(IRepo<Ticket> ticketRepository, IRepo<Model.Entities.Site> siteRepository, IUOW uow)
+    public TicketService(IRepo<Ticket> ticketRepository, IRepo<Model.Entities.Site> siteRepository, IUOW uow, ILogger<TicketService> logger, IIntegrationEventProducer eventProducer)
     {
         _ticketRepository = ticketRepository;
         _siteRepository = siteRepository;
+        _logger = logger;
         _uow = uow;
+        _eventProducer = eventProducer;
     }
     public async Task<Ticket> CreateTicketAsync(CreateTicketDTO createTicketDTO)
 
@@ -39,6 +46,20 @@ public class TicketService
         };
 
         await _ticketRepository.AddAsync(ticket);
+
+        var BookingCreatedEvent = new BookingCreatedEvent
+        {
+            Id = ticket.Id,
+            SiteName = ticket.SiteName,
+            PhoneNumber = ticket.PhoneNumber,
+            PlateNumber = ticket.PlateNumber,
+            BookingFrom = ticket.BookingFrom,
+            BookingTo = ticket.BookingTo,
+            TotalPrice = ticket.TotalPrice,
+        };
+        _eventProducer.Enqueue(BookingCreatedEvent);
+        _logger.LogInformation("Enqueued Booking Created event for Ticket  {TicketId}", ticket.Id);
+
         await _uow.SaveChangesAsync(2);
 
         return ticket;
---
+++ Invoice.Application/EventHandlers/BookingCreatedEventHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using Invoice.Model.Entities;
+using Microsoft.Extensions.Logging;
+using SharedKernel.EventDriven.Abstraction;
+using SharedKernel.Infrastructure.Persistent.Abstraction;
+using SharedKernel.MessageBus.Abstraction;
+
+namespace Invoice.Application.EventHandlers;
+
+public class BookingCreatedEventHandler : IMessageHandler<BookingCreatedEvent>
+{
+    private readonly IRepo<Ticket> _siteRepository;
+    private readonly IUOW _uow;
+    private readonly ILogger<BookingCreatedEventHandler> _logger;
+
+    public BookingCreatedEventHandler(IRepo<Ticket> siteRepository, IUOW uow, ILogger<BookingCreatedEventHandler> logger)
+    {
+        _siteRepository = siteRepository;
+        _uow = uow;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(BookingCreatedEvent message, CancellationToken ct)
+    {
+        _logger.LogInformation("Message come with site name  =  {Name}", message.SiteName);
+
+        var ticket = new Ticket
+        {
+            Id = message.Id,
+            SiteName = message.SiteName,
+            PhoneNumber = message.PhoneNumber,
+            PlateNumber = message.PlateNumber,
+            BookingFrom = message.BookingFrom,
+            BookingTo = message.BookingTo,
+            TotalPrice = message.TotalPrice,
+        };
+
+        await _siteRepository.AddAsync(ticket);
+        await _uow.SaveChangesAsync();
+    }
+
+}
